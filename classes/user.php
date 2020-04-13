c57329fa979f1fa6422f97348cbd6612cab0526c
Suman1713 <sumn1713@gmail.com>
2020-04-12 22:41:27 -0500

    Master files

---
+++ classes/user.php
@@ -0,0 +1,76 @@
+<?php
+
+class User 
+{
+    private $_db;
+
+    function __construct($db)
+    {
+    	$this->_db = $db;
+    }
+
+	private function get_user_hash($username)
+	{
+		try {
+			$stmt = $this->_db->prepare('SELECT password, username, memberID FROM members WHERE username = :username AND active="Yes" ');
+			$stmt->execute(array('username' => $username));
+
+			return $stmt->fetch();
+
+		} catch(PDOException $e) {
+		    echo '<p class="bg-danger">'.$e->getMessage().'</p>';
+		}
+	}
+
+	public function isValidUsername($username)
+	{
+		if (strlen($username) < 3) {
+			return false;
+		}
+
+		if (strlen($username) > 17) {
+			return false;
+		}
+
+		if (! ctype_alnum($username)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public function login($username, $password)
+	{
+		if (! $this->isValidUsername($username)) {
+			return false;
+		}
+
+		if (strlen($password) < 3) {
+			return false;
+		}
+
+		$row = $this->get_user_hash($username);
+
+		if (password_verify($password, $row['password'])) {
+
+		    $_SESSION['loggedin'] = true;
+		    $_SESSION['username'] = $row['username'];
+		    $_SESSION['memberID'] = $row['memberID'];
+		    
+		    return true;
+		}
+	}
+
+	public function logout()
+	{
+		session_destroy();
+	}
+
+	public function is_logged_in()
+	{
+		if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
+			return true;
+		}
+	}
+
+}
